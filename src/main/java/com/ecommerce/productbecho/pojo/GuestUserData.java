c18e6cda91260b5d90e9bbef379357cc1593e50c
Abhirup <iamabhirup7@gmail.com>
2022-04-21 23:13:06 +0530

    Checkout code

---
+++ src/main/java/com/ecommerce/productbecho/pojo/GuestUserData.java
@@ -0,0 +1,15 @@
+package com.ecommerce.productbecho.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuestUserData {
+    private String email;
+    private String phone;
+}
